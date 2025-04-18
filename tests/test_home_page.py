6c7e89b6cd8788d5d2219d2f4263e37e21cc11a3
a-l-e-x-kot <fiend69@yandex.ru>
2025-04-18 15:43:45 +0300

    пофиксила тесты для home_page

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -12,6 +12,10 @@ class TestFaq:
     @pytest.mark.parametrize('question_number, expected_answer', TestData.test_data_answer_faq)
     def test_click_faq_expand_icons_text_is_expected(self, driver, question_number, expected_answer):
         home_page = HomePage(driver)
+
+        home_page.wait_visibility_cookie()
+        home_page.close_cookie_window()
+
         home_page.scroll_to_faq()
         home_page.wait_faq_items_question_is_visible(question_number)
         home_page.click_faq_item(question_number)
